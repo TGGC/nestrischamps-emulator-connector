bc3faaab021019ec30927e5408d2d50a59a9886a
TGGC <ChristianRoesch@gmx.de>
2022-10-29 17:41:21 +0200

    Update variables.lua
    move easyconnect elsewhere

--- nestrischamps/variables.lua
+++ nestrischamps/variables.lua
@@ -4,12 +4,6 @@ pcall(function()
     log("environment.lua loaded")
 end)
 
-pcall(function()
-    require "nestrischamps.easyconnect"
-	print("easyconnect activated")
-	print("Delete nestrischamps/easyconnect.lua to disable easyconnect")
-end)
-
 PIECETABLE = {0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 3, 4, 4, 5, 5, 5, 5, 6, 6} -- corresponds to PIECEPOSITIONTABLE
 
 PIECEGRAPHICTABLE = {"01", "10", "11", "01", "10", "11", "01"} -- which graphic each piece uses
